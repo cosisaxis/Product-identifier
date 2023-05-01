1813447a01ed6bc507cc87e7ff11e6164e0c08e5
cosisaxis <kishanelewis2@gmail.com>
2023-05-01 13:35:20 -0500

    added new functions

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -9,6 +9,8 @@ contract Product {
         uint256 year;
         address owner;
         string barcode;
+        uint256 price;
+        uint256 size;
     }
 
     mapping(address => Record) private records;
@@ -17,18 +19,16 @@ contract Product {
         owner = msg.sender;
     }
 
-    modifier onlyAdmin() {
-        require(msg.sender == owner, "Only admin can call this function");
-        _;
-    }
-
-    function createOrUpdateRecord(string memory _brand, string memory _item, uint256 _year, string memory _barcode) public  {
+   
+    function createOrUpdateRecord(string memory _brand, uint256 _price, uint256 _size, string memory _item, uint256 _year, string memory _barcode) public  {
         records[msg.sender] = Record({
             brand: _brand,
             item: _item,
             year: _year,
             owner: msg.sender,
-            barcode: _barcode
+            barcode: _barcode,
+            size: _size,
+            price: _price
         });
     }
 
@@ -46,6 +46,13 @@ contract Product {
 
     function getYear(address _user) external view returns (uint256) {
         return records[_user].year;
+    }
+
+     function getSize(address _user) external view returns (uint256) {
+        return records[_user].size;
+    }
+    function getPrice(address _user) external view returns (uint256) {
+        return records[_user].price;
     }
     
     function getBarcode() external view returns (string memory) {
